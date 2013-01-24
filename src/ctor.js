865201d0bd4bc02a381dc2a884332c76835f162b
plugn <bananafishbone@gmail.com>
2013-01-24 18:15:27 +0400

    Update src/ctor.js

--- src/ctor.js
+++ src/ctor.js
@@ -5,40 +5,29 @@
 (function() {
   // root object, 'window' in the browser, or 'global' on the server.
   var root = this;
+  
+  function isArray(o) { 
+    return o instanceof Array || {}.toString.call(o) === '[object Array]';
+  }  
+  function grow(member) {
+    return 'function' == typeof member? ctor.spawn(member) : ctor.clone(member);
+  }  
 
   var ctor = {
   
-    unlink: function(o){
-      var unlinked;
-      if ('object'==typeof o) {
-        if (o.length){
-          unlinked = [];
-          for (var i = 0, l = o.length; i < l; i++) unlinked[i] = unlink(o[i]);
-        } else { 
-          unlinked = {};
-          for (var p in o) unlinked[p] = unlink(o[p]);
-        }
-      } else { return o; }
-      return unlinked;
-    },    
-    
     clone: function(o){
-      function es5(o){
-        var copy = Object.create( Object.getPrototypeOf(o) );
-        var propNames = Object.getOwnPropertyNames(o);
-        propNames.forEach(function(name){
-          var desc = Object.getOwnPropertyDescriptor(o, name);
-          Object.defineProperty(copy, name, desc);
-        });
-        return copy;
-      };  
-      function es3(o){
-        for(p in o)
-          this[p] = (typeof(o[p]) == 'object')? new arguments.callee(o[p]) : o[p];
-      };
-      
-      return  ('object'!==typeof o)? ctor.unlink(o) : (Object.create? es5(o) : new es3(o));
-    },
+      var cloned;
+      if ('object' == typeof o) {
+        if (isArray(o)){
+          cloned = [];
+          for (var i = 0, l = o.length; i < l; i++) cloned[i] = ctor.clone(o[i]);
+        } else { 
+          cloned = {};
+          for (var p in o) cloned[p] = ctor.clone(o[p]);
+        }
+      } else return o;
+      return cloned;
+    },    
     
     //  OOP Object Factory instantiates an object with own properties cloned from a prototype
     //  var o = ctor.spawn( (Function) constructor, (Array) arguments )
@@ -58,16 +47,14 @@
     // var o = ctor.inherit((Function or Object) [TopClass, ..] MidClass, BaseClass);
     inherit: function() {
       var args = [].slice.call(arguments);
-      if (args.length < 2) {
-        return false;
-      } else if (args.length > 2) {
-        for (var idx = args.length - 2, obj = args[idx + 1]; idx > -1; idx--){
+      if (args.length < 2) return false;
+      if (args.length > 2) {
+        for (var idx = args.length - 2, obj = args[idx + 1]; idx > -1; idx--)
           obj = arguments.callee(args[idx], obj);
-        }  
         return obj;
       } else {
-        var instance = (typeof args[0] == 'function') ? ctor.spawn(args[0]) : ctor.clone(args[0]),
-            parent   = (typeof args[1] == 'function') ? ctor.spawn(args[1]) : ctor.clone(args[1]);
+        var instance = grow(args[0]),
+            parent   = grow(args[1]);
         for (var k in parent) {
           if ('$parent'==k || 'undefined'==typeof parent[k])
             continue;
