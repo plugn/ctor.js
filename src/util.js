b2e99a4c92cf7e0befc019182294c82e7e731c14
plugn <bananafishbone@gmail.com>
2013-01-30 07:56:41 -0800

    getNested() added

---
+++ src/util.js
@@ -0,0 +1,21 @@
+/*
+tries to get deep nested property an object, 
+if found returns it, else returns `def` argument
+
+getNested('p.a.t.h', {p:{a:{t:{h:[1],i:[2]}}}}, '*' )
+[1]
+
+getNested('p.a.t.i', {p:{a:{t:{h:[1],i:[2]}}}}, '*' )
+[2]
+
+('p.a.t.y', {p:{a:{t:{h:[1],i:[2]}}}}, '*' )
+"*"
+
+*/
+
+var getNested = function(path, root, def){
+  var key, val = root || this, arr = String(path).split('.');
+  while (key = arr.shift())
+    val = 'undefined'==typeof val[key]? ('undefined'==typeof def? null : def) : val[key];
+  return val;  
+}  
