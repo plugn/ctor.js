4944a3bb601cf58c5dde2b17502e366a1170b5ba
plugn <bananafishbone@gmail.com>
2013-01-31 19:26:08 +0400

    arguments order changed
    Most often, the method used to obtain the values ​​of the global scope (this). 

--- src/util.js
+++ src/util.js
@@ -2,18 +2,18 @@
 tries to get deep nested property of passed object, 
 if found returns it, else returns `def` argument
 
-getNested('p.a.t.h', {p:{a:{t:{h:[1],i:[2]}}}}, '*' )
+getNested('p.a.t.h', '*', {p:{a:{t:{h:[1],i:[2]}}}} )
 [1]
 
-getNested('p.a.t.i', {p:{a:{t:{h:[1],i:[2]}}}}, '*' )
+getNested.call({p:{a:{t:{h:[1],i:[2]}}}}, 'p.a.t.i', '*' )
 [2]
 
-('p.a.t.y', {p:{a:{t:{h:[1],i:[2]}}}}, '*' )
+('p.a.t.y', '*', {p:{a:{t:{h:[1],i:[2]}}}} )
 "*"
 
 */
 
-function getNested(path, root, def){
+function getNested(path, def, root){
   var key, val = !!root? root : this, arr = String(path).split('.');
   while ((key = arr.shift()) && 'object' == typeof val && val) {
     val = 'undefined' == typeof val[key]? ('undefined' == typeof def? false : def) : val[key];
