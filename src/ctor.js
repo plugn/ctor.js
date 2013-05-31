53771ddae05ce9a653385e7fb559912cb46a62ba
Max Dolgov <bananafishbone@gmail.com>
2013-05-31 23:45:03 +0400

    Update ctor.js

--- src/ctor.js
+++ src/ctor.js
@@ -66,7 +66,33 @@
         instance.$parent = parent;
         return instance;
       }
-    }
+    },
+    /*
+        tries to get deep nested property of passed object, 
+        if found returns it, else returns `def` argument
+        
+        getNested('p.a.t.h', '*', {p:{a:{t:{h:[1],i:[2]}}}} )
+        [1]
+        getNested.call({p:{a:{t:{h:[1],i:[2]}}}}, 'p.a.t.i', '*' )
+        [2]
+        ('p.a.t.y', '*', {p:{a:{t:{h:[1],i:[2]}}}} )
+        "*"
+    */
+    getNested: function(path, def, root){
+      var key, val = !!root? root : this, arr = String(path).split('.');
+      while ((key = arr.shift()) && 'object' == typeof val && val) {
+        val = 'undefined' == typeof val[key]? ('undefined' == typeof def? false : def) : val[key];
+      }
+      return val;  
+    },
+      
+    // Function.bind() aka Currying micro implementaion
+    curry: function(fn, context){
+      var args = [].slice.call(arguments, 2);
+      return function(){ 
+        fn.apply(context, args.concat([].slice.call(arguments))) 
+      };
+    }    
   }
   
   root.ctor = ctor;
