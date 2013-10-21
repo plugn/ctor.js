290292fbf1970fc6a4317bf6dbdca5713c97bd11
Max Dolgov <bananafishbone@gmail.com>
2013-10-22 00:13:29 +0400

    Create fnclone.js

---
+++ src/fnclone.js
@@ -0,0 +1,32 @@
+/*
+function a1(x,y){ var X = arguments[0], Y = arguments[1]; return X + Y;  }; a1.prop = 'PROP-a1';
+"PROP-a1"
+a1 
+function a1(x,y){ var X = arguments[0], Y = arguments[1]; return X + Y;  }
+a1.toString()
+"function a1(x,y){ var X = arguments[0], Y = arguments[1]; return X + Y;  }"
+var b1 = Function(a1.toString());
+undefined
+b1
+function anonymous() {
+function a1(x,y){ var X = arguments[0], Y = arguments[1]; return X + Y;  }
+}
+b1.prop 
+undefined
+a2 = a1;
+function a1(x,y){ var X = arguments[0], Y = arguments[1]; return X + Y;  }
+a2(3,5)
+8
+a1(2,1)
+3
+b1(7,8)
+undefined
+a1.toString()
+"function a1(x,y){ var X = arguments[0], Y = arguments[1]; return X + Y;  }"
+a1.toString().match()
+[""]
+a1.toString().replace(/^\s|\s$/g,'')
+"function a1(x,y){ var X = arguments[0], Y = arguments[1]; return X + Y;  }"
+*/
+
+a1.toString().replace(/^\s|\s$/g,'')
