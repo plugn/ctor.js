dc4e24e1b9e6673b13d8f0c9d7f368668393997a
Max Dolgov <bananafishbone@gmail.com>
2013-10-22 15:11:28 +0400

    Update cloneFunc.js

--- src/cloneFunc.js
+++ src/cloneFunc.js
@@ -5,7 +5,7 @@
  */
 
 function cloneFunc( func ) {
-  var reFn = /^function\s+([^\s(]*)\s*\(([^)]*)\)[^{]*\{([^]*)\}$/gi
+  var reFn = /^function\s*([^\s(]*)\s*\(([^)]*)\)[^{]*\{([^]*)\}$/gi
     , s = func.toString().replace(/^\s|\s$/g, '')
     , m = reFn.exec(s);
   if (!m || !m.length) return; 
