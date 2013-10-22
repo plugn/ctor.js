dc3e6ac76b677f1c00c7b86d17f242af0e891c00
Max Dolgov <bananafishbone@gmail.com>
2013-10-22 14:21:07 +0400

    Update and rename fnclone.js to cloneFunc.js

---
+++ src/cloneFunc.js
@@ -0,0 +1,20 @@
+/**
+ *  @title cloning a Function
+ *  @author  Max L Dolgov 
+ *  #contact bananafishbone at gmail dot com
+ */
+
+function cloneFunc( func ) {
+  var reFn = /^function\s+([^\s(]*)\s*\(([^)]*)\)[^{]*\{([^]*)\}$/gi
+    , s = func.toString().replace(/^\s|\s$/g, '')
+    , m = reFn.exec(s);
+  if (!m || !m.length) return; 
+  var conf = {
+      name : m[1] || '',
+      args : m[2].replace(/\s+/g,'').split(','),
+      body : m[3] || ''
+  }
+  var clone = Function.prototype.constructor.apply(this, [].concat(conf.args, conf.body));
+  return clone;
+}
+
