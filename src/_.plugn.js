b61f8c5d509a6482993c4f824d219af82e41c818
Max Dolgov <bananafishbone@gmail.com>
2014-01-17 20:11:06 +0300

    Update _.plugn.js

--- src/_.plugn.js
+++ src/_.plugn.js
@@ -3,40 +3,49 @@
 // @url http://github.com/plugn
 
 _.mixin({
-	capitalize: function(string) {
-		return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
-	},
+    clone: function clone(o) {
+        if (!(this instanceof clone)) {
+            return new clone(o); 
+        }
+        for(p in o) {
+            this[p] = 'object' == typeof o[p] ? new clone(o[p]) : o[p]; 
+        }
+    },
+    
+    capitalize: function(string) {
+        return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
+    },
 
-	trim: function(val) {
-		return String(val).replace(/(^\s+|\s+$)/g, ''); 
-	},
+    trim: function(val) {
+        return String(val).replace(/(^\s+|\s+$)/g, ''); 
+    },
 
-	toNumber: function(t){
-		var a; return (a = String(t).match(/\d+/g)) && (parseInt(a.join(''), 10) || 0);
-	},
+    toNumber: function(t){
+        var a; return (a = String(t).match(/\d+/g)) && (parseInt(a.join(''), 10) || 0);
+    },
 
-	// results hash-items {key:value} 
-	filterHash: function(obj, iterator, context) {
-		var results = {};
-		if (obj == null) return results;
-		_.each(obj, function(value, index, list) {
-			// var hVal = {}; hVal[index] = value;
-			if (iterator.call(context, value, index, list)) results[index] = value;
-		});
-		return results;
-	},
-	rejectHash:  function(obj, iterator, context) {
-		return _.filterHash(obj, function(value, index, list) {
-			return !iterator.call(context, value, index, list);
-		}, context);
-	},
+    // results hash-items {key:value} 
+    filterHash: function(obj, iterator, context) {
+        var results = {};
+        if (obj == null) return results;
+        _.each(obj, function(value, index, list) {
+            // var hVal = {}; hVal[index] = value;
+            if (iterator.call(context, value, index, list)) results[index] = value;
+        });
+        return results;
+    },
+    rejectHash:  function(obj, iterator, context) {
+        return _.filterHash(obj, function(value, index, list) {
+            return !iterator.call(context, value, index, list);
+        }, context);
+    },
 
-	getNested: function(path, def, root){
-		var key, val = !!root? root : this, arr = String(path).split('.');
-		while ((key = arr.shift()) && 'object' == typeof val && val) {
-			val = 'undefined' == typeof val[key]? ('undefined' == typeof def? false : def) : val[key];
-		}
-		return val;  
-	}  
+    getNested: function(path, def, root){
+        var key, val = !!root? root : this, arr = String(path).split('.');
+        while ((key = arr.shift()) && 'object' == typeof val && val) {
+            val = 'undefined' == typeof val[key]? ('undefined' == typeof def? false : def) : val[key];
+        }
+        return val;  
+    }  
 
 });
