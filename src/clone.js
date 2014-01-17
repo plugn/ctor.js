69f12f69eefc6da2caddb0823d7007d3d56a7e28
Max Dolgov <bananafishbone@gmail.com>
2014-01-17 19:44:41 +0300

    Update clone.js

--- src/clone.js
+++ src/clone.js
@@ -1,8 +1,8 @@
 function clone(o) {
   if (!(this instanceof clone)) {
-  	return new clone(o); 
+    return new clone(o); 
   }
   for(p in o) {
-  	this[p] = 'object' === typeof o[p] ? (new clone(o[p])) : o[p]; 
+    this[p] = 'object' == typeof o[p] ? new clone(o[p]) : o[p]; 
   }
 }
